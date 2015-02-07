f5ac6cd8eaf1a4434a092046441d8c152fd83708
Bhavika Devnani <bhavika.devnani@gmail.com>
2015-02-07 11:57:38 -0500

    first commit of partially working sobriety test1

--- src/app.js
+++ src/app.js
@@ -1,48 +1,58 @@
-console.log('Simply.js demo!');
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
 
-simply.on('singleClick','select', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  simply.subtitle('Ow');
+var UI = require('ui');
+var Vector2 = require('vector2');
+var Accel = require('ui/accel');
+var ajax = require('ajax');
+
+
+
+
+var main = new UI.Card({
+  title: 'ARE YOU DRUNK?',
+  icon: 'images/menu_icon.png',
+  subtitle: 'FIND OUT!',
+  body: 'Press Select.'
 });
 
-simply.on('singleClick','up', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  simply.subtitle('Howd you feel if someone poked YOU in the head');
+main.show();
+
+main.on('click', 'select', function(e) {
+  var wind = new UI.Window();
+  var textfield = new UI.Text({
+    position: new Vector2(0, 50),
+    size: new Vector2(144, 30),
+    font: 'gothic-24-bold',
+    text: 'Test 1: Coordination',
+    textAlign: 'center'
+  });
+  wind.add(textfield);
+  wind.show();
+  wind.on('click', 'select', function(e) {
+    textfield.size(new Vector2(130,30));
+    var sum, sum1, sum2, sum3, n;
+    sum = 0;
+    sum1 = 0;
+    sum2 = 0;
+    sum3 = 0;
+    n = 0;
+    wind.on('accelData', function(e) {
+      // var x = e.accel.x;
+      // var y = e.accel.y;
+      // var z = JSON.stringify(e.accel.z);
+      sum1 += e.accel.x*e.accel.x;
+      sum2 += e.accel.y*e.accel.y;
+      sum3 += (e.accel.z + 1000)*(e.accel.z + 1000);
+      n++;
+      
+      textfield.text("Average: " + Math.sqrt(sum1/n) + ' ' + Math.sqrt(sum2/n) + ' ' + Math.sqrt(sum3/n)); // '(' + x + ", " + y + ", " + ", " + z + ")"
 });
-
-simply.on('singleClick','down', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  simply.subtitle('Getting down right dirty');
+      
+      
+    
+  });
 });
-
-simply.on('longClick','select', function(e) {
-  console.log(util2.format('long clicked $button!', e));
-  simply.subtitle('OUCH');
-  simply.vibe();
-  simply.scrollable(e.button !== 'select');
-});
-
-simply.on('longClick','up', function(e) {
-  console.log(util2.format('long clicked $button!', e));
-  simply.subtitle('Watchu getting out of this');
-  simply.vibe();
-  simply.scrollable(e.button !== 'select');
-});
-
-simply.on('longClick','down', function(e) {
-  console.log(util2.format('long clicked $button!', e));
-  simply.subtitle('Dropping to a new low...');
-  simply.vibe();
-  simply.scrollable(e.button !== 'select');
-});
-
-simply.on('accelTap', function(e) {
-  console.log(util2.format('tapped accel axis $axis $direction!', e));
-  simply.subtitle('Honestly, now youre just being rude');
-});
-
-simply.text({
-  title: 'Dont push my buttons...',
-  subtitle: '(hold em for different spans of time)',
-}, true);
-
